0da5fc00c653cf814b3e5b9972c5dd1d8a15b682
spojchil <2069510482@qq.com>
2026-05-06 22:18:31 +0800

    fix(vul): 删除main合并带入的11个旧版vul-service文件，已在新架构中重写

--- vul-service/vul-business/src/main/java/com/hawkeye/vul/business/mapper/VulCategoryMappingMapper.java
+++
@@ -1,9 +0,0 @@
-package com.hawkeye.vul.business.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.hawkeye.vul.common.pojo.entity.VulCategoryMapping;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface VulCategoryMappingMapper extends BaseMapper<VulCategoryMapping> {
-}
